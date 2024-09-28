1228b47fe8fd24fc2206166479cd361d25f83929
Jenny Seburn <jennifer.seburn@gmail.com>
2024-09-28 14:14:46 -0400

    additional setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  plugins: [react(), svgr(), vanillaExtractPlugin()],
+});
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "json"],
+      exclude: ["&&/.*", "**/*.d.ts"],
+    },
+  },
+});
