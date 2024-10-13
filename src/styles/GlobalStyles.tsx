70f7ba873169837cfd75e736715775c348ab628b
Jenny Seburn <jennifer.seburn@gmail.com>
2024-10-13 12:55:24 -0400

    edit global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,5 +1,26 @@
-import { Global, css } from "@emotion/react";
+import { Global, css, useTheme } from "@emotion/react";
 
 export default function GlobalStyles() {
-  return <Global styles={css``} />;
+  const theme = useTheme();
+  return (
+    <Global
+      styles={css({
+        "html, body, #root": {
+          margin: 0,
+          padding: 0,
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+          fontFamily: theme.font.standard.family,
+          fontSize: theme.font.standard.size.regular,
+        },
+        "#root": {
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+      })}
+    />
+  );
 }
