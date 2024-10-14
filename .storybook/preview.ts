180e1a50c1761158527e1f47f3e0726e218d54dc
Jenny Seburn <jennifer.seburn@gmail.com>
2024-10-13 22:13:24 -0400

    add theme modes to chromatic

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 import GlobalStyles from "../src/styles/GlobalStyles";
 import { ThemeProvider } from "@emotion/react";
 import { light, dark } from "../src/styles/theme";
+import { allModes } from "./modes";
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +11,12 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    chromatic: {
+      modes: {
+        light: allModes.light,
+        dark: allModes.dark,
       },
     },
   },
